b2ed548c9ba96a7945b94b141cf3f6e95a7d0873
woniu <50673488@qq.com>
2015-02-05 17:36:52 +0900

    20150204 1604 V1

--- index1.php
+++ index1.php
@@ -1,2 +1,12 @@
-V1
-V2
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
